76443b7e72ec99ecec54e654cdefdb44fc32f5c3
Andy Clement <aclement@pivotal.io>
2018-09-17 11:56:07 -0700

    Fix module generation and target-classes locator

--- application/src/main/java/app/main/SampleApplication.java
+++ application/src/main/java/app/main/SampleApplication.java
@@ -2,31 +2,31 @@ package app.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.ApplicationContextInitializer;
+//import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.GenericApplicationContext;
 
 @SpringBootConfiguration
 @Import({ SampleConfiguration.class })
 public class SampleApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(SampleApplication.class);
 		app.setApplicationContextClass(GenericApplicationContext.class);
 		app.run(args);
 	}
 
-	public static ApplicationContextInitializer<GenericApplicationContext> initializer() {
-		return new Initializer();
-	}
-
-	private static class Initializer
-			implements ApplicationContextInitializer<GenericApplicationContext> {
-
-		@Override
-		public void initialize(GenericApplicationContext context) {
-			context.registerBean(SampleApplication.class);
-		}
-
-	}
+//	public static ApplicationContextInitializer<GenericApplicationContext> initializer() {
+//		return new Initializer();
+//	}
+//
+//	private static class Initializer
+//			implements ApplicationContextInitializer<GenericApplicationContext> {
+//
+//		@Override
+//		public void initialize(GenericApplicationContext context) {
+//			context.registerBean(SampleApplication.class);
+//		}
+//
+//	}
 }
