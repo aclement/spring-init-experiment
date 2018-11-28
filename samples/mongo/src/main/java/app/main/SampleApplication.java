02850627fbbfe6c32b6841bec3b896a289b8044d
Dave Syer <dsyer@pivotal.io>
2018-11-28 16:23:08 +0000

    Add stats in CLR

--- samples/mongo/src/main/java/app/main/SampleApplication.java
+++ samples/mongo/src/main/java/app/main/SampleApplication.java
@@ -1,5 +1,9 @@
 package app.main;
 
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
@@ -50,9 +54,14 @@ public class SampleApplication {
 	}
 
 	@Bean
-	public CommandLineRunner runner() {
+	public CommandLineRunner runner(ConfigurableListableBeanFactory beans) {
 		return args -> {
 			foos.findAll().switchIfEmpty(foos.save(new Foo("Hello"))).subscribe();
+			System.err.println("Class count: " + ManagementFactory.getClassLoadingMXBean()
+					.getTotalLoadedClassCount());
+			System.err.println("Bean count: " + beans.getBeanDefinitionNames().length);
+			System.err.println(
+					"Bean names: " + Arrays.asList(beans.getBeanDefinitionNames()));
 		};
 	}
 
