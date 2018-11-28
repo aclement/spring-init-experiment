fc592200999f7dd27be52c66fcac5953d0de88a2
Dave Syer <dsyer@pivotal.io>
2018-11-28 15:14:50 +0000

    Update to reactive Mongo

--- samples/mongo/src/main/java/app/main/SampleApplication.java
+++ samples/mongo/src/main/java/app/main/SampleApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.context.ConfigurationPropertiesAutoConfiguration;
 import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
-import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.reactive.ReactiveWebServerFactoryAutoConfiguration;
@@ -19,15 +19,12 @@ import org.springframework.boot.autoconfigure.web.reactive.error.ErrorWebFluxAut
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.web.reactive.function.server.RouterFunction;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
-
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 /**
  * You need to start Mongo locally to run the app (e.g. use the docker-compose.yml
@@ -39,11 +36,11 @@ import reactor.core.scheduler.Schedulers;
 @SpringBootConfiguration
 @Import({ PropertyPlaceholderAutoConfiguration.class,
 		ConfigurationPropertiesAutoConfiguration.class,
-		EmbeddedMongoAutoConfiguration.class, MongoAutoConfiguration.class,
-		MongoDataAutoConfiguration.class, MongoRepositoriesAutoConfiguration.class,
-		JacksonAutoConfiguration.class, ReactiveWebServerFactoryAutoConfiguration.class,
-		WebFluxAutoConfiguration.class, ErrorWebFluxAutoConfiguration.class,
-		HttpHandlerAutoConfiguration.class })
+		EmbeddedMongoAutoConfiguration.class, MongoReactiveAutoConfiguration.class,
+		MongoReactiveDataAutoConfiguration.class,
+		MongoReactiveRepositoriesAutoConfiguration.class, JacksonAutoConfiguration.class,
+		ReactiveWebServerFactoryAutoConfiguration.class, WebFluxAutoConfiguration.class,
+		ErrorWebFluxAutoConfiguration.class, HttpHandlerAutoConfiguration.class })
 public class SampleApplication {
 
 	private CustomerRepository foos;
@@ -55,19 +52,13 @@ public class SampleApplication {
 	@Bean
 	public CommandLineRunner runner() {
 		return args -> {
-			Foo foo = foos.findAll().isEmpty() ? null : foos.findAll().iterator().next();
-			if (foo == null) {
-				foos.save(new Foo("Hello"));
-			}
+			foos.findAll().switchIfEmpty(foos.save(new Foo("Hello"))).subscribe();
 		};
 	}
 
 	@Bean
 	public RouterFunction<?> userEndpoints() {
-		return route(GET("/"),
-				request -> ok()
-						.body(Mono.fromCallable(() -> foos.findAll().iterator().next())
-								.subscribeOn(Schedulers.elastic()), Foo.class));
+		return route(GET("/"), request -> ok().body(foos.findAll().single(), Foo.class));
 	}
 
 	public static void main(String[] args) {
@@ -82,5 +73,5 @@ public class SampleApplication {
 
 }
 
-interface CustomerRepository extends MongoRepository<Foo, String> {
+interface CustomerRepository extends ReactiveCrudRepository<Foo, String> {
 }
