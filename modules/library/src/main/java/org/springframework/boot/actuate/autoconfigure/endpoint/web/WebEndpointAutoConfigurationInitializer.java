c8d571d5df214d97079428c066c6c39a68e75a19
Dave Syer <dsyer@pivotal.io>
2018-12-07 10:37:38 +0000

    Fix compiler error (works in Eclipse but not with Maven)

--- modules/library/src/main/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/WebEndpointAutoConfigurationInitializer.java
+++ modules/library/src/main/java/org/springframework/boot/actuate/autoconfigure/endpoint/web/WebEndpointAutoConfigurationInitializer.java
@@ -1,8 +1,12 @@
 package org.springframework.boot.actuate.autoconfigure.endpoint.web;
 
-import java.lang.Override;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
+
+import slim.ConditionService;
+import slim.ImportRegistrars;
+
 import org.springframework.boot.actuate.autoconfigure.endpoint.ExposeExcludePropertyEndpointFilter;
 import org.springframework.boot.actuate.endpoint.EndpointFilter;
 import org.springframework.boot.actuate.endpoint.EndpointsSupplier;
@@ -16,34 +20,91 @@ import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpointDisco
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.ResolvableType;
-import slim.ConditionService;
-import slim.ImportRegistrars;
 
-public class WebEndpointAutoConfigurationInitializer implements ApplicationContextInitializer<GenericApplicationContext> {
-  @Override
-  public void initialize(GenericApplicationContext context) {
-    ConditionService conditions = context.getBeanFactory().getBean(ConditionService.class);
-    if (conditions.matches(WebEndpointAutoConfiguration.class)) {
-      if (context.getBeanFactory().getBeanNamesForType(WebEndpointAutoConfiguration.class).length==0) {
-        new WebEndpointAutoConfiguration_WebEndpointServletConfigurationInitializer().initialize(context);
-        context.getBeanFactory().getBean(ImportRegistrars.class).add(WebEndpointAutoConfiguration.class, "org.springframework.boot.context.properties.EnableConfigurationPropertiesImportSelector");
-        context.registerBean(WebEndpointAutoConfiguration.class, () -> new WebEndpointAutoConfiguration(context,context.getBean(WebEndpointProperties.class)));
-        context.registerBean("webEndpointPathMapper", PathMapper.class, () -> context.getBean(WebEndpointAutoConfiguration.class).webEndpointPathMapper());
-        if (conditions.matches(WebEndpointAutoConfiguration.class, EndpointMediaTypes.class)) {
-          context.registerBean("endpointMediaTypes", EndpointMediaTypes.class, () -> context.getBean(WebEndpointAutoConfiguration.class).endpointMediaTypes());
-        }
-        if (conditions.matches(WebEndpointAutoConfiguration.class, WebEndpointDiscoverer.class)) {
-          context.registerBean("webEndpointDiscoverer", WebEndpointDiscoverer.class, () -> context.getBean(WebEndpointAutoConfiguration.class).webEndpointDiscoverer(context.getBean(ParameterValueMapper.class),context.getBean(EndpointMediaTypes.class),context.getBeanProvider(PathMapper.class),context.getBeanProvider(OperationInvokerAdvisor.class),context.getBeanProvider(ResolvableType.forClassWithGenerics(EndpointFilter.class, EndpointFilter.class))));
-        }
-        if (conditions.matches(WebEndpointAutoConfiguration.class, ControllerEndpointDiscoverer.class)) {
-          context.registerBean("controllerEndpointDiscoverer", ControllerEndpointDiscoverer.class, () -> context.getBean(WebEndpointAutoConfiguration.class).controllerEndpointDiscoverer(context.getBeanProvider(PathMapper.class),context.getBeanProvider(ResolvableType.forClassWithGenerics(Collection.class, Collection.class))));
-        }
-        if (conditions.matches(WebEndpointAutoConfiguration.class, PathMappedEndpoints.class)) {
-          context.registerBean("pathMappedEndpoints", PathMappedEndpoints.class, () -> context.getBean(WebEndpointAutoConfiguration.class).pathMappedEndpoints(context.getBeanProvider(EndpointsSupplier.class).stream().collect(Collectors.toList()),context.getBean(WebEndpointProperties.class)));
-        }
-        context.registerBean("webExposeExcludePropertyEndpointFilter", ExposeExcludePropertyEndpointFilter.class, () -> context.getBean(WebEndpointAutoConfiguration.class).webExposeExcludePropertyEndpointFilter());
-        context.registerBean("controllerExposeExcludePropertyEndpointFilter", ExposeExcludePropertyEndpointFilter.class, () -> context.getBean(WebEndpointAutoConfiguration.class).controllerExposeExcludePropertyEndpointFilter());
-      }
-    }
-  }
+public class WebEndpointAutoConfigurationInitializer
+		implements ApplicationContextInitializer<GenericApplicationContext> {
+	@SuppressWarnings("unchecked")
+	@Override
+	public void initialize(GenericApplicationContext context) {
+		ConditionService conditions = context.getBeanFactory()
+				.getBean(ConditionService.class);
+		if (conditions.matches(WebEndpointAutoConfiguration.class)) {
+			if (context.getBeanFactory().getBeanNamesForType(
+					WebEndpointAutoConfiguration.class).length == 0) {
+				new WebEndpointAutoConfiguration_WebEndpointServletConfigurationInitializer()
+						.initialize(context);
+				context.getBeanFactory().getBean(ImportRegistrars.class).add(
+						WebEndpointAutoConfiguration.class,
+						"org.springframework.boot.context.properties.EnableConfigurationPropertiesImportSelector");
+				context.registerBean(WebEndpointAutoConfiguration.class,
+						() -> new WebEndpointAutoConfiguration(context,
+								context.getBean(WebEndpointProperties.class)));
+				context.registerBean("webEndpointPathMapper", PathMapper.class,
+						() -> context.getBean(WebEndpointAutoConfiguration.class)
+								.webEndpointPathMapper());
+				if (conditions.matches(WebEndpointAutoConfiguration.class,
+						EndpointMediaTypes.class)) {
+					context.registerBean("endpointMediaTypes", EndpointMediaTypes.class,
+							() -> context.getBean(WebEndpointAutoConfiguration.class)
+									.endpointMediaTypes());
+				}
+				if (conditions.matches(WebEndpointAutoConfiguration.class,
+						WebEndpointDiscoverer.class)) {
+					context.registerBean("webEndpointDiscoverer",
+							WebEndpointDiscoverer.class,
+							() -> context.getBean(WebEndpointAutoConfiguration.class)
+									.webEndpointDiscoverer(
+											context.getBean(ParameterValueMapper.class),
+											context.getBean(EndpointMediaTypes.class),
+											context.getBeanProvider(PathMapper.class),
+											context.getBeanProvider(
+													OperationInvokerAdvisor.class),
+											context.getBeanProvider(
+													ResolvableType.forClassWithGenerics(
+															EndpointFilter.class,
+															EndpointFilter.class))));
+				}
+				if (conditions.matches(WebEndpointAutoConfiguration.class,
+						ControllerEndpointDiscoverer.class)) {
+					context.registerBean("controllerEndpointDiscoverer",
+							ControllerEndpointDiscoverer.class,
+							() -> context.getBean(WebEndpointAutoConfiguration.class)
+									.controllerEndpointDiscoverer(
+											context.getBeanProvider(PathMapper.class),
+											context.getBeanProvider(
+													ResolvableType.forClassWithGenerics(
+															Collection.class,
+															Collection.class))));
+				}
+				if (conditions.matches(WebEndpointAutoConfiguration.class,
+						PathMappedEndpoints.class)) {
+					context.registerBean("pathMappedEndpoints", PathMappedEndpoints.class,
+							() -> context.getBean(WebEndpointAutoConfiguration.class)
+									.pathMappedEndpoints(
+											collection(context
+													.getBeanProvider(
+															EndpointsSupplier.class)
+													.stream()
+													.collect(Collectors.toList())),
+											context.getBean(
+													WebEndpointProperties.class)));
+				}
+				context.registerBean("webExposeExcludePropertyEndpointFilter",
+						ExposeExcludePropertyEndpointFilter.class,
+						() -> context.getBean(WebEndpointAutoConfiguration.class)
+								.webExposeExcludePropertyEndpointFilter());
+				context.registerBean("controllerExposeExcludePropertyEndpointFilter",
+						ExposeExcludePropertyEndpointFilter.class,
+						() -> context.getBean(WebEndpointAutoConfiguration.class)
+								.controllerExposeExcludePropertyEndpointFilter());
+			}
+		}
+	}
+
+	// Generics hack...
+	@SuppressWarnings("rawtypes")
+	private Collection collection(List<EndpointsSupplier> list) {
+		return list;
+	}
+
 }
